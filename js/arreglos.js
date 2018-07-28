a18cebdc6e9a59a08e3d65a2921fa7dfaade98e5
Leo Picado <leopic@gmail.com>
2018-07-27 22:07:38 -0600

    Mejora de posible parseo fallando

--- js/arreglos.js
+++ js/arreglos.js
@@ -34,8 +34,8 @@ function readCookie(name) {
     var ca = document.cookie.split(';');
     for (var i = 0; i < ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0) == ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
     }
     return null;
 }
