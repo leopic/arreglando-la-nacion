0f8e1df0f92ef471e76e2d12cbdcebaf87867b50
Leo Picado <leopic@gmail.com>
2018-08-17 17:00:58 -0600

    Remueve entradas duplicadas de mas leidas

--- js/arreglos.js
+++ js/arreglos.js
@@ -1,54 +1,56 @@
-var conteoDeCookies = 0;
+var removerCookies = function () {
+    var conteoDeCookies = 0;
 
-// Remueve los cookies que llevan un registro de la cantidad de historias leidas
-// http://stackoverflow.com/questions/2144386/javascript-delete-cookie
-function createCookie(name, value, days) {
-    var expires = '';
-    if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toGMTString();
+    // Remueve los cookies que llevan un registro de la cantidad de historias leidas
+    // http://stackoverflow.com/questions/2144386/javascript-delete-cookie
+    function createCookie(name, value, days) {
+        var expires = '';
+        if (days) {
+            var date = new Date();
+            date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+            expires = "; expires=" + date.toGMTString();
+        }
+
+        document.cookie = name + "=" + value + expires + "; path=/";
     }
 
-    document.cookie = name + "=" + value + expires + "; path=/";
-}
+    function readCookie(name) {
+        var nameEQ = name + "=";
+        var ca = document.cookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+            if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        }
 
-function readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+        return null;
     }
 
-    return null;
-}
-
-function eraseCookie(name) {
-    createCookie(name, "", -1);
-    conteoDeCookies++;
-}
-
-// El nombre del cookie es un hash, entonces en vez de adivinarlo,
-// recorremos los cookies y los borramos todos 🎉️
-var allCookies = document.cookie.split(';');
-for (var j = 0; j < allCookies.length; j++) {
-    var name = allCookies[j].split('=')[0].trim(),
-        value = readCookie(name);
-
-    try {
-        value = decodeURIComponent(value.replace(/\+/g, " "));
-        eraseCookie(name);
-    } catch (e) {
+    function eraseCookie(name) {
+        createCookie(name, "", -1);
+        conteoDeCookies++;
     }
-}
 
-if (conteoDeCookies) {
-    console.log('aln: removimos %i cookies', conteoDeCookies);
-}
+    // El nombre del cookie es un hash, entonces en vez de adivinarlo,
+    // recorremos los cookies y los borramos todos 🎉️
+    var allCookies = document.cookie.split(';');
+    for (var j = 0; j < allCookies.length; j++) {
+        var name = allCookies[j].split('=')[0].trim(),
+            value = readCookie(name);
 
-window.onload = function () {
+        try {
+            value = decodeURIComponent(value.replace(/\+/g, " "));
+            eraseCookie(name);
+        } catch (e) {
+        }
+    }
+
+    if (conteoDeCookies) {
+        console.log('aln: removimos %i cookies', conteoDeCookies);
+    }
+};
+
+var permitirSeleccionarTexto = function () {
     window.setTimeout(function () {
         try {
             if (jQuery !== undefined) {
@@ -57,4 +59,30 @@ window.onload = function () {
         } catch (e) {
         }
     }, 5000);
+};
+
+var removerArticulosRepetidos = function () {
+    var titular = document.querySelectorAll('.headline-hed-last')[0];
+    var masLeidos = document.querySelectorAll('.most-read h4 a');
+    var numeros = document.querySelectorAll('.most-read h1');
+
+    for (var j = 0; j < numeros.length; j++) {
+        numeros[j].classList = 'hidden';
+    }
+
+    if (titular) {
+        for (var i = 0; i < masLeidos.length; i++) {
+            var esteTitular = masLeidos[i];
+
+            if (esteTitular.innerText.trim() === titular.innerText.trim()) {
+                esteTitular.parentNode.parentNode.parentNode.classList = 'hidden';
+            }
+        }
+    }
+};
+
+window.onload = function () {
+    removerCookies();
+    permitirSeleccionarTexto();
+    removerArticulosRepetidos();
 };
